67de55574c40b9722b444859a4821697319c3595
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-01-28 08:27:07 -0300

    feat: WIP transform workflow

--- internal/image/model/image_model.go
+++ internal/image/model/image_model.go
@@ -13,7 +13,31 @@ type ImageEntity struct {
 type TransformReq struct {
 	UUID uuid.UUID `json:"id"`
 
-	// TODO: Add the transformation... for now I only have the UUID
+	Resize    *ResizeOptions `json:"resize,omitempty"`
+	Crop      *CropOptions   `json:"crop,omitempty"`
+	Compress  *string        `json:"compress,omitempty"`
+	Watermark *bool          `json:"watermark,omitempty"`
+	Mirror    *bool          `json:"mirror,omitempty"`
+	Rotate    *int           `json:"rotate,omitempty"`
+	Format    *string        `json:"format,omitempty"`
+	Filters   *FilterOptions `json:"filters,omitempty"`
+}
+
+type ResizeOptions struct {
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
+type CropOptions struct {
+	Width  int `json:"width"`
+	Height int `json:"height"`
+	X      int `json:"x"`
+	Y      int `json:"y"`
+}
+
+type FilterOptions struct {
+	Grayscale bool `json:"grayscale,omitempty"`
+	Sepia     bool `json:"sepia,omitempty"`
 }
 
 type ImageResp struct {
