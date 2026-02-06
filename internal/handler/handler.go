ffee575d05cdb0e995068ee13b72081a636fac3e
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-02-06 09:56:11 -0300

    fix: I forgot about this one lol

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,13 +2,13 @@ package handler
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/rafaeldepontes/imagopher/internal/application/model"
+	"github.com/rafaeldepontes/imagopher/internal/application"
 	"github.com/rafaeldepontes/imagopher/internal/images/processor/controller"
 )
 
 // I'm thinking of building my on Radix Tree to not use the
 // chi package and use mine instead... But I don't want it
 // now, but I'm planning...
-func Routes(r *chi.Mux, app *model.Application) {
+func Routes(r *chi.Mux, app application.Application) {
 	controller.MapImageRoutes(r, app.ImageController)
 }
